62f82e6319d55a02d8b141bce00e904d9b729ed4
putri02 <putriyantipurba16@gmail.com>
2026-08-19 08:05:10 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -218,7 +218,7 @@ input:-webkit-autofill:focus {{
 .stTabs [data-baseweb="tab"] *,
 .stTabs [role="tab"] * {{
     font-weight: 700 !important;
-    font-size: 0.88rem !important;
+    font-size: 0.95rem !important;
     white-space: nowrap !important;
 }}
 .stTabs [data-baseweb="tab"]:hover:not([aria-selected="true"]),
@@ -250,10 +250,11 @@ input:-webkit-autofill:focus {{
 }}
 
 /* ---- Sub-tab bersarang (Random Forest / LightGBM / Performa Model): menyempit sesuai isi ---- */
-[data-baseweb="tab-panel"] [data-baseweb="tab-list"],
-[data-baseweb="tab-panel"] div[role="tablist"] {{
+[data-testid="stTabs"] [role="tabpanel"] [data-baseweb="tab-list"],
+[data-testid="stTabs"] [role="tabpanel"] div[role="tablist"] {{
     display: inline-flex !important;
     width: auto !important;
+    max-width: fit-content !important;
 }}
 
 /* ---- Metrics ---- */
